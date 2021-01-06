130e398852af1e2c352474aede5df6cd9ac05891
Momen Kamal <themonx11@gmail.com>
2021-01-06 20:57:53 +0000

    updated SPI_Programmer

--- SPI_Programmer.py
+++ SPI_Programmer.py
@@ -4,9 +4,25 @@ import numpy as np
 
 import time
 
+def write_spi (port,data):
+    a =  data[0:2]
+    b =  data[2:4]
+    c =  '0'+ data[4:5]
+    d =  data[5:7]
+    X = bytearray.fromhex(a+b)
+    Y = bytearray.fromhex(c+d)
+
+    port.write(X)
+    port.write(Y) 
+    time.sleep(0.1)
+    port.write(bytearray(b'\x00\x00')) 
+    time.sleep(0.1)
+    pass
 
 
-ser = serial.Serial('COM5', 8*1000000, timeout=2)  # open serial port
+
+
+ser = serial.Serial('COM3', 8*1000000, timeout=2)  # open serial port
 ser.flushInput()
 ser.flushOutput()
 ser.write(bytearray(b'\xff\xff')) # Choose mode/reset
@@ -14,45 +30,15 @@ ser.write(bytearray(b'\x00\x04')) # set mode to beamforming
 # ser.write(bytearray(b'\x00\x00')) # This is needed some
 
 
-# time.sleep(1)
-
 # Write Mode 
-ser.write(bytearray(b'\x20\x00')) 
-ser.write(bytearray(b'\x00\x00')) 
-
-time.sleep(0.1)
-
-ser.write(bytearray(b'\x00\x00')) 
-# ser.write(bytearray(b'\x00\x00')) 
-
-time.sleep(0.1)
+write_spi(ser, '2000000')
 
 # Write data
-ser.write(bytearray(b'\x20\x20')) 
-ser.write(bytearray(b'\x00\x00')) 
-
-time.sleep(0.1)
-
-ser.write(bytearray(b'\x00\x00')) 
-# ser.write(bytearray(b'\x00\x00')) 
-time.sleep(0.1)
+write_spi(ser, '2020000')
 
 # Read Mode 
-ser.write(bytearray(b'\x20\x00')) 
-ser.write(bytearray(b'\x00\x02')) 
+write_spi(ser, '2000002')
 
-time.sleep(0.1)
-
-ser.write(bytearray(b'\x00\x00')) 
-# ser.write(bytearray(b'\x00\x00')) 
-
-time.sleep(0.1)
 # read data
-ser.write(bytearray(b'\x20\x20')) 
-ser.write(bytearray(b'\x01\x00')) 
+write_spi(ser, '2020000')
 
-time.sleep(0.1)
-
-ser.write(bytearray(b'\x00\x00')) 
-time.sleep(0.1)
-# ser.write(bytearray(b'\x00\x00')) 
